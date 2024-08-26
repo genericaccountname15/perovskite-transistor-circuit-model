380bd78507d34577a8112ad414b735e5ee2eaa17
timothy <Timothy.chew22@imperial.ac.uk>
2024-08-26 11:30:39 +0100

    Fixed nobias support

--- Guesser.py
+++ Guesser.py
@@ -14,10 +14,28 @@ import scipy.special as sps
 import Finder as find
 from FinderInterface import Interface
 
-def param_guesser(bias_data, nobias_data, IV_data, bias_voltage, kbt=4.1302114835e-21, Js=2.31e-16, bias=True, nanoparticles=False, run_checker=True):
-    #unpacking
+def param_guesser(bias_data, nobias_data, IV_data, bias_voltage, kbt=4.1302114835e-21,
+                   Js=2.31e-16, bias=True, nanoparticles=False, run_checker=True):
+    """
+    Calculates parameter guess values from features found from Finder module
+    Args:
+        bias_data (array):
+        nobias_data (array):
+        IV_data (array or None):
+        bias_voltage (float):
+        kbt (float): 
+        Js (float):
+        bias (boolean):
+        nanoparticles (boolean):
+        run_checker (boolean):
+    Returns:
+        param_list_bias (tuple, if bias):
+        param_list_nobias (tuple, if no bias):
+    """
 
-    Rion, Rsh, Rs, Rn0, Rninf, wg_bias, wg_nobias, wion_bias, wion_nobias, wnano = Interface(bias_data, nobias_data, IV_data, run_checker=run_checker, nanoparticle=nanoparticles)
+    #unpacking variables
+    Rion, Rsh, Rs, Rn0, Rninf, wg_bias, wg_nobias, wion_bias, wion_nobias, wnano = Interface(
+        bias_data, nobias_data, IV_data, run_checker=run_checker, nanoparticle=nanoparticles)
 
     #solving for variable values
     Cion_bias = find.get_Cion(wion_bias, Rion)
@@ -41,10 +59,10 @@ def param_guesser(bias_data, nobias_data, IV_data, bias_voltage, kbt=4.130211483
 
     if nanoparticles:
         param_list_bias = (CA_ratio, Cg_bias, Cion_bias, Rion, kbt, n, Js, bias_voltage, Rs, Rsh, Rnano, Cnano)
-        param_list_nobias = (CA_ratio, Cg_nobias, Cion_nobias, Rion, kbt, n, Js, bias_voltage, Rs, Rsh, Rnano, Cnano)
+        param_list_nobias = (CA_ratio, Cg_nobias, Cion_nobias, Rion, kbt, n, Js, 0.001, Rs, Rsh, Rnano, Cnano)
     else:
         param_list_bias = (CA_ratio, Cg_bias, Cion_bias, Rion, kbt, n, Js, bias_voltage, Rs, Rsh)
-        param_list_nobias = (CA_ratio, Cg_nobias, Cion_nobias, Rion, kbt, n, Js, bias_voltage, Rs, Rsh)
+        param_list_nobias = (CA_ratio, Cg_nobias, Cion_nobias, Rion, kbt, n, Js, 0.001, Rs, Rsh)
 
     if bias:
         return param_list_bias
@@ -74,10 +92,10 @@ if __name__ == "__main__":
     nobias_data = np.loadtxt("test_data/nyquist2_dark.txt", skiprows=1)
     bias_voltage = 1.023
 
-    plist = param_guesser(bias_data, nobias_data, IV_data, bias_voltage, run_checker=False, bias=True, nanoparticles=True)
+    plist = param_guesser(bias_data, nobias_data, IV_data, bias_voltage, run_checker=False, bias=False, nanoparticles=True)
 
     from nanoparticles_model.Impedancefunction import Z
     from Plotter import plotter
-    plotter(Z, "nanoparticles_model\Initial_params.csv", bias_data, plist)
+    plotter(Z, "nanoparticles_model\Initial_params.csv", nobias_data, plist)
 
     
